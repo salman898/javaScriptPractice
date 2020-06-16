15a621cc4c08f00be9d114abfd5359bf0fb3a48e
salman898 <s.t.khan701@gmail.com>
2020-06-16 17:36:25 +0500

    added new code

--- chapter5.js
+++ chapter5.js
@@ -22,5 +22,118 @@ document.write("Sum of "+a+ " and " +b+ " is = " ,result);*/
 // var result = Number(a) / Number(b);
 // document.write("Divide of "+a+ " and " +b+ " is = " ,result);
 
-var  a ;
-a = 3;
+/*3. Do the following using JS Mathematic Expressions a. Declare a variable. */
+
+/*var  a ;
+document.write("Value after variable declaration is ",typeof a);
+a = 6;
+document.write("<br>Initial Value = ",a);
+
+a++;
+document.write("<br>After Increment the Value = ",a);
+var c = a+7;
+document.write("<br>Value after addition = " , c);
+c--;
+document.write("<br>Value after Decrement = ",c);
+var d = c / 3;
+document.write("<br>The Remender is = ",d);*/
+
+/*4. Cost of one movie ticket is 600 PKR. Write a script to store ticket price in
+ a variable & calculate the cost of buying 5 tickets to a movie.*/
+
+ /*var ticketPrice = 600;        //One Ticket price 
+ var total = ticketPrice * 5;  //5 ticket price 
+ document.write("Total cost to buy 5 tickets to a movie is = " +total+ "PKR");*/
+
+//  5. Write a script to display multiplication table of any number in your browser. E.g 
+ /*var anyNumber = 5;
+ document.write("Table of " , anyNumber + "<br></br>");
+ document.write(anyNumber + "x" + "1" + "=" + anyNumber*1 + "<br></br>");
+ document.write(anyNumber + "x" + "2" + "=" + anyNumber*2 + "<br></br>");
+ document.write(anyNumber + "x" + "3" + "=" + anyNumber*3 + "<br></br>");
+ document.write(anyNumber + "x" + "4" + "=" + anyNumber*4 + "<br></br>");
+ document.write(anyNumber + "x" + "5" + "=" + anyNumber*5 + "<br></br>");
+ document.write(anyNumber + "x" + "6" + "=" + anyNumber*6 + "<br></br>");
+ document.write(anyNumber + "x" + "7" + "=" + anyNumber*7 + "<br></br>");
+ document.write(anyNumber + "x" + "8" + "=" + anyNumber*8 + "<br></br>");
+ document.write(anyNumber + "x" + "9" + "=" + anyNumber*9  + "<br></br>");
+ document.write(anyNumber + "x" + "10" + "=" + anyNumber*10 + "<br></br>");*/
+
+//  7.  Write a program to implement checkout process of a shopping cart system for an e-commerce website. 
+//  Store the following in variables 
+
+/*document.write("<h1>Shopping Cart</h1>")
+var PriceofItem_1 = 600 , PriceofItem_2 = 300;
+var OrederofItem1 = 3 , Orderofitem2 = 6 , shippingcharges = 200;
+document.write("Price of Item 1 is " + PriceofItem_1);
+document.write("<br>Quantity of 1 is " + 3);
+document.write("<br>Price of Item 1 is " + PriceofItem_2);
+document.write("<br>Quantity of 2 is " + 3);
+document.write("<br>Shipping Charges " + shippingcharges);
+var total = OrederofItem1*PriceofItem_1+Orderofitem2*PriceofItem_2+shippingcharges;
+document.write("<br><br>Total cost of your order is = Rs " + total);*/
+
+
+// 8. Store total marks & marks obtained by a student in 2 variables. 
+// Compute the percentage & show the result in your browse
+
+/*document.write("<h1>Marks Sheet</h1>")
+var obtaintedMarks = +prompt("Enter Total Maeks ");
+var totalMarks = +prompt("Enter Total Maeks ");
+document.write("Total Marks = " + totalMarks);
+document.write("<br>Obtained Marks = "+ obtaintedMarks);
+var p = obtaintedMarks/totalMarks*100;
+document.write("<br>Percentage = " +p+"%");*/
+
+
+// 9. Assume we have 10 US dollars & 25 Saudi Riyals. Write a script to convert the total currency to Pakistani Rupees.
+//  Perform all calculations in a single expression. 
+
+/*var Dollar = 10;
+var SaudiRiyal = 25;
+var total = (Dollar*104.80) + (SaudiRiyal*28);
+document.write("<h1>Currency in PKR</h1>");
+document.write("Total Currency in PKR: "+total);*/
+
+// 10. Write a program to initialize a variable with some number and do 
+// arithmetic in following sequence:
+
+/*var num1 = 20;
+var result = (num1+5)*10/2;
+document.write(result);*/
+
+
+// 1. The Age Calculator: Forgot how old someone is? Calculate it!
+
+/*var curntYear = 2020;
+var birthYear = 1997;
+document.write("<br>Current Year : "+curntYear);
+document.write("<br>Birth year : "+birthYear);
+var age = curntYear-birthYear;
+document.write("<br>Your age is: "+age);*/
+
+// 12. The Geometrizer: Calculate properties of a circle. a. Store a radius into a variable.
+
+/*document.write("<h1>The Geometorize</h1>")
+var Rcircle = 20;
+document.write("Radius of a Circle: "+Rcircle)
+var π = 3.142;
+var cir = 2*π*Rcircle;
+document.write("<br>The Circumferrence is: "+cir);
+var area =  π*Rcircle*Rcircle;
+document.write("<br>The area is: "+area);*/
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
